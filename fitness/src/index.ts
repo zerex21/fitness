595fc25ec38e861ced054ed71072bd157131e403
vadim-sid1993 <79473969+vadim-sid1993@users.noreply.github.com>
2023-02-05 21:49:27 +0300

    feat: add new pages
    pages: training, training_search and programms.

--- fitness/src/index.ts
+++ fitness/src/index.ts
@@ -1,1 +1,8 @@
 import './style.css';
+import { trainingCategoryAll, openCloseList } from './scripts/training/openCloseList';
+import { trainingArrowLeftAll, trainingArrowRightAll, shiftLeft, shiftRight } from './scripts/training/shift';
+
+trainingArrowRightAll.forEach((el, i) => el.addEventListener("click", () => shiftLeft(i)));
+trainingArrowLeftAll.forEach((el, i) => el.addEventListener("click", () => shiftRight(i)));
+
+trainingCategoryAll.forEach((el, i) => el.addEventListener("click", () => openCloseList(i)));
---
+++ fitness/src/scripts/training/shift.ts
@@ -0,0 +1,23 @@
+const trainingArrowLeftAll = document.querySelectorAll(".training_arrow_left") as NodeListOf<HTMLElement>;
+const trainingArrowRightAll = document.querySelectorAll(".training_arrow_right") as NodeListOf<HTMLElement>;
+const sliderContainerAll = document.querySelectorAll(".training_list") as NodeListOf<HTMLElement>;
+let countAll = new Array(3).fill(0);
+let positionAll = new Array(3).fill(0);
+
+function shiftLeft(i: number) {
+    if (countAll[i] < 3) {
+        positionAll[i] -= 620;
+        sliderContainerAll[i].style.transform = `translateX(${positionAll[i]}px)`;
+        countAll[i] += 1;
+    };
+}
+
+function shiftRight(i: number) {
+    if (countAll[i] > 0) {
+        positionAll[i] += 620;
+        sliderContainerAll[i].style.transform = `translateX(${positionAll[i]}px)`;
+        countAll[i] -= 1;
+    };
+}
+
+export { trainingArrowLeftAll, trainingArrowRightAll, shiftLeft, shiftRight };
