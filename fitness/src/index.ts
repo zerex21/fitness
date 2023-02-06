d32cd13485189e2377394145b6c4c767bc139cf5
vadim-sid1993 <79473969+vadim-sid1993@users.noreply.github.com>
2023-02-06 16:56:00 +0300

    fix: fix training page

--- fitness/src/index.ts
+++ fitness/src/index.ts
@@ -1,8 +1,12 @@
 import './style.css';
 import { trainingCategoryAll, openCloseList } from './scripts/training/openCloseList';
 import { trainingArrowLeftAll, trainingArrowRightAll, shiftLeft, shiftRight } from './scripts/training/shift';
+import { buttonForYou, buttonSearch, clickSearch, clickForYou } from './scripts/training/clickButton';
 
 trainingArrowRightAll.forEach((el, i) => el.addEventListener("click", () => shiftLeft(i)));
 trainingArrowLeftAll.forEach((el, i) => el.addEventListener("click", () => shiftRight(i)));
 
 trainingCategoryAll.forEach((el, i) => el.addEventListener("click", () => openCloseList(i)));
+
+buttonForYou.addEventListener("click", clickForYou);
+buttonSearch.addEventListener("click", clickSearch);
--- fitness/src/scripts/training/openCloseList.ts
+++ fitness/src/scripts/training/openCloseList.ts
@@ -1,7 +1,7 @@
 const trainingCategoryAll = document.querySelectorAll(".training_category_composite") as NodeListOf<HTMLElement>;
 const trainingCategoryListAll = document.querySelectorAll(".training_category_list") as NodeListOf<HTMLElement>;
 
-function openCloseList(i: number) {
+function openCloseList(i: number): void {
     trainingCategoryListAll[i].classList.toggle("training_active");
 }
 
